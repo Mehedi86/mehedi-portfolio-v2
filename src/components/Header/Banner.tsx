ca81cf90edbe337d5bb52e0770d0f52aa4d37ffc
Md. Abdullah Al Mehedi <abdullahalmehedi86@gmail.com>
2025-09-08 13:29:54 +0600

    added: banner design complete

--- src/components/Header/Banner.tsx
+++ src/components/Header/Banner.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { MdOutlineContacts } from "react-icons/md";
 import { GoProjectSymlink } from "react-icons/go";
 import { SiReaddotcv } from "react-icons/si";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa";
+import { SiHackerrank } from "react-icons/si";
+import Marquee from "react-fast-marquee";
+import { FaReact, FaNodeJs } from "react-icons/fa";
+import { SiMongodb, SiNextdotjs, SiTailwindcss, SiJavascript } from "react-icons/si";
 
 export default function Banner() {
     const buttonClass = "bg-white text-black px-2 py-1 rounded cursor-pointer flex items-center gap-2 hover:scale-105 transition-transform duration-300"
@@ -16,10 +23,20 @@ export default function Banner() {
 
             {/* Content */}
             <div className="px-4">
+                <div className="md:w-1/2 mx-auto flex justify-center items-center text-3xl lg:text-4xl text-blue-500 my-12">
+                    <div className='flex gap-6'>
+                        <FaReact title="React" />
+                        <SiNextdotjs title="Next.js" />
+                        <FaNodeJs title="Node.js" className="text-green-600" />
+                        <SiMongodb title="MongoDB" className="text-green-500" />
+                        <SiTailwindcss title="Tailwind CSS" className="text-sky-500" />
+                        <SiJavascript title="JavaScript" className="text-yellow-500" />
+                    </div>
+                </div>
                 <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">Hey, I'm Mehedi <br />
                     A Software Developer</h1>
-                <p className="mt-4 text-lg text-gray-700 dark:text-gray-300">
-                    A Fullstack Developer passionate about building creative applications. I love to innovate new designs and share beautiful, dynamic websites with the world.
+                <p className="mt-4 text-lg text-gray-700 dark:text-gray-300 font-[500]">
+                    A <span className='font-bold'>Fullstack Developer</span> passionate about building creative applications. I love to innovate new designs and share beautiful, dynamic websites with the world.
                 </p>
 
                 <div>
@@ -29,8 +46,16 @@ export default function Banner() {
 
                     <div className='my-4 flex gap-4 flex-wrap'>
                         <button className={`${buttonClass}`}><MdOutlineContacts size={16} /> Contact Me</button>
-                        <button className={`${buttonClass}`}><GoProjectSymlink size={16}/>View Projects</button>
-                        <button className={`${buttonClass}`}><SiReaddotcv size={16}/>Download Resume</button>
+                        <button className={`${buttonClass}`}><GoProjectSymlink size={16} />View Projects</button>
+                        <button className={`${buttonClass}`}><SiReaddotcv size={16} />Download Resume</button>
+                    </div>
+                    {/* social links */}
+                    <div className='flex w-1/2 md:w-1/3 mx-auto justify-between items-center my-12'>
+                        <FaGithub size={28} />
+                        <FaLinkedin size={28} />
+                        <div className='bg-black w-0.5 h-10'></div>
+                        <FaDiscord size={32} />
+                        <SiHackerrank size={28} />
                     </div>
                 </div>
             </div>
